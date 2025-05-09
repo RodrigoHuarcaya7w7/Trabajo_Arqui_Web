0409e2ff1d563280714759674af5c4ac8326f0c1
palomaaa18 <palomaromerosan@gmail.com>
2025-05-08 23:20:33 -0500

    Update Detalle_Pedido.java

--- src/main/java/pe/edu/upc/trabajoparcial/entities/Detalle_Pedido.java
+++ src/main/java/pe/edu/upc/trabajoparcial/entities/Detalle_Pedido.java
@@ -19,4 +19,8 @@ public class Detalle_Pedido {
 
     @Column(name = "nombre", nullable = false, length = 100)
     private String nombre;
+
+    @Column(name = "precio", nullable = false)
+    private float precio;
+
 }
