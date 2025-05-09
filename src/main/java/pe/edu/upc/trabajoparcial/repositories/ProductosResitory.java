c9d0a00e1e0d628c60c88d19fe4831ea6f58c40c
palomaaa18 <palomaromerosan@gmail.com>
2025-05-08 22:17:59 -0500

    listarPorRangoPrecios

--- src/main/java/pe/edu/upc/trabajoparcial/repositories/ProductosResitory.java
+++ src/main/java/pe/edu/upc/trabajoparcial/repositories/ProductosResitory.java
@@ -5,5 +5,8 @@ import pe.edu.upc.trabajoparcial.entities.Productos;
 import pe.edu.upc.trabajoparcial.entities.Roles;
 
 public interface ProductosResitory extends JpaRepository<Productos, Long> {
-
+//verificar//
+  //QUERY JPQL: buscar producto por rango de precios
+  @Query("SELECT p FROM Producto p WHERE p.precio BETWEEN :min AND :max")
+    List<Productos> buscarPorRangoDePrecio(float min, float max)
 }
