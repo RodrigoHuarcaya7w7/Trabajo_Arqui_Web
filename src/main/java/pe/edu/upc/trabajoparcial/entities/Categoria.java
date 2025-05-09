68cde068c7fbdfa9a76547804c34a31ff63c08bc
Stefany2404 <alepc17242010@gmail.com>
2025-05-08 23:57:48 -0500

    Update Categoria.java

--- src/main/java/pe/edu/upc/trabajoparcial/entities/Categoria.java
+++ src/main/java/pe/edu/upc/trabajoparcial/entities/Categoria.java
@@ -19,4 +19,7 @@ public class Categoria {
 
     @Column(name = "nombre", nullable = false, length = 100)
     private String nombre;
+
+    @Column(name = "descripción", length = 500) 
+    private String descripción; 
 }
